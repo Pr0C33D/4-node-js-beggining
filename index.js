f903f59392fa52e95479e055e9174e4ed23047ee
Pr0C33D <77962630+Pr0C33D@users.noreply.github.com>
2021-10-12 19:53:59 +0300

    Funkciju dokumentacija

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const data = require('./lib/data.js');
 
-console.log(data);
-
-data.create();
-data.read();
+data.create('users', 'petras', { name: 'Petras', age: 99 });
+data.read('users', 'petras');
 data.update();
 data.delete();
+
+data.create('books', 'du-gaideliai', { text: 'tekstas' });
