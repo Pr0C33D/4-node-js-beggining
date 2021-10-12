a6043ada55561bf22d183747c66f747edebfbe17
Pr0C33D <77962630+Pr0C33D@users.noreply.github.com>
2021-10-12 21:37:25 +0300

    First function done

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const data = require('./lib/data.js');
 
-data.create('users', 'petras', { name: 'Petras', age: 99 });
-data.read('users', 'petras');
-data.update();
-data.delete();
+data.create('users', 'petras', { name: 'Petras', age: 99 }, (state, msg) => {
+    
+    if (state) {
+        console.log(msg);
+    } else {
+        console.error(msg);
+    }
+    console.log('Sekantys zingsniai po bandymo sukurti faila...');
+});
 
-data.create('books', 'du-gaideliai', { text: 'tekstas' });
